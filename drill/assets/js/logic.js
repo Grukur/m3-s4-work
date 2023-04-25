8ca33d860d73f81cedc615160452afec88755d41
Darold K. Trench <darold.trench@gmail.com>
2023-04-25 18:57:54 -0400

    Delete drill directory
    drill off

--- drill/assets/js/logic.js
+++
@@ -1,9 +0,0 @@
-
-$('#button').click(function(event){
-    event.preventDefault()
-    if($('#check').prop('checked') && $('#email').val() != ''){
-        alert('Felicitaciones! Pronto escucharás de nosotros')
-    }else{
-        alert('Parece que nos faltó algo...')
-    }
-})
